2260639c614f8b8a92c23dbf60e69f32be26dad8
Nabil <sith.lord.ndark@icloud.com>
2017-12-28 16:02:49 +0000

    Uploaded files

---
+++ main.cpp
@@ -0,0 +1,30 @@
+//============================================================================
+// Name        : 0_Exercises.cpp
+// Author      : 
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+void greet ();
+
+int main() {
+	greet();
+	return 0;
+}
+
+void greet (){
+	int Greet{0};
+	cout << "Please enter a Number:	";
+	cin >> Greet;
+	if(Greet > 0 ){
+		for (int i = 0;  i < Greet; ++i) {
+
+			cout << "Welcome on board!"<<endl;
+		}
+	}else{
+		cout << "Fuck off!" <<endl;
+	}
+}
