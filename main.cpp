5066df187950de1e4c92ffbc50d22c5ffc692e14
Nabil <sith.lord.ndark@icloud.com>
2017-12-28 17:54:10 +0000

    new version

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : 0_Exercises.cpp
 // Author      : Nabil
-// Version     : 1.0
+// Version     : 3.0
 // Copyright   : OFC
 // Description : Hello World in C++, Ansi-style
 //============================================================================
@@ -9,10 +9,36 @@
 #include <iostream>
 using namespace std;
 void greet ();
+double calculus(int , float);
 
 int main() {
+	int a;
+	float b;
+	double sum;
 	greet();
+	cout << "Enter table's cases: ";
+	cin >> a;
+	cout << "Enter the multiplicator a float is permitted: ";
+	cin >> b;
+	calculus (a,b);
+	sum = calculus (a;b);
+	cout << sum;
 	return 0;
+}
+
+double calculus (int a, float b) {
+	double arr[a];
+	double sum{0};
+	for (int i = 0; i < a; ++i) {
+		cout << "Please enter Number "<< i+1 << " : " << endl;
+		cin >> arr[i];
+		arr[i] *= b;
+	}
+	for (int j = 0; j < a; ++j) {
+		cout << arr [j] <<" ";
+		sum += arr[j];
+	}
+	return sum;
 }
 
 void greet (){
